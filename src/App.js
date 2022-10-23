8f11a5d4388e607aebd44ffc8833af19734b946f
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-10-23 18:44:42 +0300

    fix JSX

--- src/App.js
+++ src/App.js
@@ -9,24 +9,18 @@ import './scss/app.scss';
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
-						<Sort/>
+            <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <PizzaBlock/>
-						<PizzaBlock/>
-						<PizzaBlock/>
-						<PizzaBlock/>
-						<PizzaBlock/>
-						<PizzaBlock/>
-						<PizzaBlock/>
-						<PizzaBlock/>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Чизбургер-пицца" price="395" />
+						<PizzaBlock title="Tema-пицца" price="675" />
           </div>
         </div>
       </div>
