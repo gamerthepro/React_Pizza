9a9d883e0054ab8ee30de407d9f03a39331cbfad
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-11-09 02:30:21 +0300

    адаптивная вёрстка

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -13,7 +13,8 @@ function PizzaBlock({ imageUrl, title, types, sizes, price }) {
   };
 
   return (
-    <div className="pizza-block">
+    <div className="pizza-block-container">
+			<div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
@@ -60,6 +61,7 @@ function PizzaBlock({ imageUrl, title, types, sizes, price }) {
         </div>
       </div>
     </div>
+		</div>
   );
 }
 
